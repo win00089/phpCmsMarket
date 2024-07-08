7d74bad7cbbf20c1026b242dfbce670fbdbe4fe0
win00089 <bf109e1111@gmail.com>
2024-07-08 14:40:40 +0300

    Структура проекта

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('VG_ACCESS') or die('Access denied');
+
+const SITE_URL = 'http://im.my';
+const PATH = '/';
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'im';
